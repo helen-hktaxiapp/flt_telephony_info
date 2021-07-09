a4f7e4885bacedb689db5a9581a893276a0cb078
helen@hktaxiapp.com/ <helen@hktaxiapp.com>
2021-07-09 17:10:34 +0800

    Bug fix

--- lib/flt_telephony_info.dart
+++ lib/flt_telephony_info.dart
@@ -10,9 +10,7 @@ class FltTelephonyInfo {
   static Future<TelephonyInfo> get info async {
 
     final TelephonyInfo telephonyInfo =
-        TelephonyInfo.fromMap(await (_channel.invokeMapMethod<String, dynamic>('getFlutterTelephony') as FutureOr<Map<String, dynamic>>));
-
-        
+        TelephonyInfo.fromMap(await (_channel.invokeMapMethod<String, dynamic>('getTelephonyInfo') as FutureOr<Map<String, dynamic>>));
 
     return telephonyInfo;
   }
